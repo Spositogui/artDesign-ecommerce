c19caea35ed462f674286d4bf889558d1df7f758
Spositogui <developersposito@gmail.com>
2019-06-10 22:59:57 -0300

    Produtos, submenu (side out) & Border footer

--- js/main.js
+++ js/main.js
@@ -13,3 +13,37 @@ closeMenu = () => {
 	document.getElementById('open-menu').
 	classList.remove('active-menu');
 }
+
+open_Menu_Produtos = () => {
+	document.getElementById('menu-a').
+	classList.add('active-produtos');
+
+	document.getElementById('menu-b').
+	classList.add('active-produtos');
+}
+
+back_Menu_A = () => {
+	document.getElementById('menu-a').
+	classList.remove('active-produtos');
+
+	document.getElementById('menu-b').
+	classList.remove('active-produtos');
+}
+
+colecaoDrop = () => {
+	let colecao = document.getElementById('menu-b-colecao');
+
+	if(colecao.classList.contains('drop'))
+		colecao.classList.remove("drop");
+	else
+		colecao.classList.add("drop");
+}
+
+materialDrop = () => {
+	let material = document.getElementById('menu-b-material');
+
+	if(material.classList.contains('drop'))
+		material.classList.remove("drop");
+	else
+		material.classList.add("drop");
+}
